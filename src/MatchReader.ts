be3f016550dabf036c1de3134f698d15fb9ef477
Piotr Wawrzynkiewicz <pio.wawrzynkiewicz@gmail.com>
2019-10-15 11:52:39 +0200

    Added static method

--- src/MatchReader.ts
+++ src/MatchReader.ts
@@ -1,6 +1,7 @@
 import { dateStringToDate } from './utils';
 import { MatchData } from './MatchData';
 import { MatchResult } from './MatchResult';
+import { CsvFileReader } from './CsvFileReader';
 
 interface DataReader {
   read(): void;
@@ -9,6 +10,10 @@ interface DataReader {
 
 export class MatchReader {
   matches: MatchData[] = [];
+
+  static fromCsv(fileName: string) {
+    return new MatchReader(new CsvFileReader(fileName));
+  }
 
   constructor(public reader: DataReader) {}
 
--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,7 @@ import { WinsAnalysis } from './analyzers/WinsAnalisys';
 import { Summary } from './Summary';
 import { HtmlReport } from './reportTargets/HtmlReport';
 
-const csvFileReader = new CsvFileReader('football.csv');
-
-const matchReader = new MatchReader(csvFileReader);
-matchReader.load();
+const matchReader = MatchReader.fromCsv('football.csv');
 
 const summary = new Summary(
   new WinsAnalysis('Man United'),
